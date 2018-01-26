33ce06a2705cde22bfc166f4982b5aa46ef9aef9
Anson varghese <anzonvarghese.com>
2018-01-26 12:57:30 +0530

    Button added in prescription

--- app/src/main/java/com/zibbix/medpharmapp/medipharm/PrescriptionActivity.java
+++ app/src/main/java/com/zibbix/medpharmapp/medipharm/PrescriptionActivity.java
@@ -2,12 +2,20 @@ package com.zibbix.medpharmapp.medipharm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class PrescriptionActivity extends AppCompatActivity {
-
+    EditText prescriptiontxt;
+Button nxtbutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prescription);
+
+        prescriptiontxt=(EditText)findViewById(R.id.prestxt);
+nxtbutton=(Button)findViewById(R.id.button2);
+
+
     }
 }
